aa9fb3f60ce0f66b99d35b8d16bc8d4eec60bd91
Sasha Friedenberg <sashafriedenberg@me.com>
2012-08-29 19:36:25 -0400

    roughly implement AATiledContentView

--- Scores/AATiledContentView.h
+++ Scores/AATiledContentView.h
@@ -10,9 +10,30 @@
 
 @interface AATiledContentView : UIView
 {
+    UIScrollView *scrollView;
+    CGRect previouslyVisibleRect;
     CGRect visibleRect;
+    
+    NSMutableArray *spareTiles;
+    
+    NSMutableSet *visibleTileKeys;
+    NSMutableDictionary *visibleTiles;
+    NSMutableSet *selectedTileKeys;
 }
 
+@property (nonatomic, readonly, assign) UIScrollView *scrollView;
 @property (nonatomic) CGRect visibleRect;
+
+//tiling
+
+- (NSSet *)visibleTileKeysForRect:(CGRect)rect;
+
+- (id)tile;
+- (BOOL)visibilityForTileKey:(id <NSCopying>)key;
+- (CGRect)frameForTileKey:(id <NSCopying>)key;
+- (void)prepareTileForReuse:(id)tile withKey:(id <NSCopying>)key;
+
+- (void)tileWillAppear:(id)tile withKey:(id <NSCopying>)key;
+- (void)tileDidDisappear:(id)tile withKey:(id <NSCopying>)key;
 
 @end
